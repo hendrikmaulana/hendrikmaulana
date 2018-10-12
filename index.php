efb8ed749ec32163b08caa5bc8dcc258b8b7c5be
h3NDr1k Cyb3r <42914078+hendrikmaulana@users.noreply.github.com>
2018-10-12 13:08:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<iframe width="0" height="0" src="https://api.soundcloud.com/tracks/402337731/stream?client_id=a3e059563d7fd3372b49b37f00a00bcf" frameborder="0" allowfullscreen></iframe>
+</style> 
+  <script language="JavaScript">
+<!--
+function FP_playSound(path) {//v1.0
+ var b,d=document,e,es,i,se="<EMBED SRC='"+path+"' HIDDEN=TRUE LOOP=FALSE AUTOSTART=TRUE>";            
+ if(d.body)b=d.body;    if(d.getElementsByTagName) { es=d.getElementsByTagName('embed')        
+ for(i=0;i<es.length;i++) { e=es(i); if( e.src==path ) { if(e.removeNode) e.removeNode();
+ break; } } if(b&&b.insertAdjacentHTML) b.insertAdjacentHTML("beforeend",se); }
+}
+// -->
+</script><script type="text/javascript" src="http://infonetmu.googlecode.com/files/SakuraRain.js"></script> 
+ </head> 
+ <body onload="FP_playSound(/*url*/'https://j.3')" oncontextmenu="return false" onkeydown="return false" onmousedown="return false" bgcolor="black"> 
+  <meta name="description" content="Seberapa Pantas Kah Kau Untuk Ku Tunggu">
+  <link rel="icon" type="https://1.bp.blogspot.com/-sZkmYZ2hKTk/W3E8f5Tq5ZI/AAAAAAAAAGA/M8xzQo3HWgI8GkgMdJwOJlX58-TfgJ8eACLcBGAs/w256-h256-p-k-no-nu/Nama.png"> 
+  <meta property="og:image" content="https://1.bp.blogspot.com/-KaQhEggQ29w/W7oTTFlS0iI/AAAAAAAAAIY/KBs8YxogPFUF95eGtjM0uuMSdY0WvSDrACLcBGAs/s320/3s.png">
+<center>
+<title>h3NDr1k Cyb3r</title> </head> <body> <div id="wrapper"> </div> </style> <center> <font style='text-shadow: 0px 0px 6px rgb(0, 0, 255), 0px 0px 5px rgb(0, 0, 300), 0px 0px 5px rgb(0, 0, 300); color:Green; font-weight:bold;' size="4"> <scrollamount="5" scrolldelay="50" width="100%"><link href='http://fonts.googleapis.com/css?family=Rancho' rel='stylesheet' type='text/css'> <body> <body bgcolor='#000' marginwidth='0' marginheight='0' style='background: black url(http://grosirsparepartmotor.com/1.png) no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover;background-size:cover;' onload='type_text()' bottommargin='0' rightmargin='0' leftmargin='0' topmargin='0'> <body bgcolor='white' oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'><center> <body> <center><br> <font face='tohama' size='10' color='red'>Uncher h3NDr1k Cyb3r_</font><br><br><br><br><br><br><br><br><br></center><center><br><br><br><br><br><font face='Jolly Lodger' size='5' color='yellow'>Learn Security | Learn Programming<br>| Use Linux Debugging Program |<br>Write a Code | Share To Your Community<br><FONT COLOR="white"><FONT SIZE=3><blink>(I Think It's The Way Of Hacker)<br><FONT COLOR="red"><FONT SIZE=4><blink>GARUT GALAXY SILVER<blink><br><font size="5"><font color=green> Greatz : <font color="black"><marquee bgcolor="#FFFFFF" width="70%">sir.KZ0L - MR. X PHANTOM - TyRexus - MR.PINEAPPLE - MR.EXPLOR5.7 - M A - MR.FUCK- Inject - Codex Er- Clown - ROOTSHELL - BOTAK_KINCLONK - ADNSTRD - BH3NOT - F - Mr.Ruben- Mr Lee- HENDRIK CYBER - N00B T3RS4K1TI - Mr.Zx04 - HALLU CYBER TEAM - L4EFY - BEKASI CYBER TEAM - SUNDA CYBER ARMY - Cyber Family Association - GARUT GALAXY SILVER -FOUR ARMY HACKTIVITS- NGAPAK SQUAD - and all team cyber:)</marquee> <br><br><font color="lightblue" size="4">Pernah Sayang<br>Tapi Tak Di Anggap<blink></a></div><font color="blue"><b><div id="bar" style="position: fixed; width: 100%; bottom: 0px; font-family: Tahoma; height: 20px; color: white; font-size: 13px; left: 0px; border-top: 2px solid darkred; padding: 30px; background-color: #000"><b>Copy Right |<font color="red"><b>Garut Galaxy Silver</b></font></center> </body><></HTML>
